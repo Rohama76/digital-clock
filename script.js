2402caa98942b0b9cc724681fb33df554749c343
ROHAMA SHAFIQUE <115659870+Rohama76@users.noreply.github.com>
2023-12-20 22:20:33 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours().toString().padStart(2, '0');
+    var minutes = now.getMinutes().toString().padStart(2, '0');
+    var seconds = now.getSeconds().toString().padStart(2, '0');
+    var timeString = hours + ':' + minutes + ':' + seconds;
+
+    document.getElementById('clock').innerText = timeString;
+  }
+
+  setInterval(updateClock, 1000);
+  updateClock();
